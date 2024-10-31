05e237f8ac0f00f5ec9a03015c1707b7ab69c3d3
Guilh3rme06 <164666171+Guilh3rme06@users.noreply.github.com>
2024-10-31 12:13:05 +0000

    Add files via upload
    Insert data through input

---
+++ dados.py
@@ -0,0 +1,16 @@
+#
+# Exercicio: 1
+# Autor: Guilherme Rodrigues
+# Dia: 10/10/2024
+#
+
+
+#Recolher Dados
+name = input('Qual o seu nome: ')
+age = input('Qual a sua idade: ')
+consult = input('Qual a sua consulta medica: ')
+date = input('Qual a data da sua consulta: ')
+
+#Inserir dados
+txt = f'\nO(a) {name} com a idade de {age} vai ter consulta de {consult} no dia {date}'
+print(txt)
