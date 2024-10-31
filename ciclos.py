926ee829649033f3072552c884e1a59383ad4637
Guilh3rme06 <164666171+Guilh3rme06@users.noreply.github.com>
2024-10-31 12:15:02 +0000

    Add files via upload
    Cycle for

---
+++ ciclos.py
@@ -0,0 +1,20 @@
+#
+# Listas e Tuplas --- Estruturas de dados ordenados
+# Ciclos For
+#
+# Nome- Guilherme Rodrigues
+# Data- 17/10/2024
+# 22405041
+#
+
+users = ['Gui' , 'Tiago', 'Ana', 'Rita', 'Luana'] # Podemos inserir mais itens para a lista e o for vai listar todos sem precisar de ser alterado
+
+
+for user in users: #stateman
+    age = input("Indica me a tua idade ") #Bloco de codigo
+    print(f"{user} Tem {age} anos") #Bloco de codigo
+
+# Ao fazer este for simplificamos e nao somos obrigados a usar estes prints todos
+#print(users[0])
+#print(users[1])
+#print(users[2])
